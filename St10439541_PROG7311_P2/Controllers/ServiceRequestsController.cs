862caf1f432bcb691fadfceb54cbf14603521759
st10439541 <161297175+st10439541@users.noreply.github.com>
2026-04-19 19:25:17 +0200

    Created Controllers

---
+++ St10439541_PROG7311_P2/Controllers/ServiceRequestsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using St10439541_PROG7311_P2.Data;
+using St10439541_PROG7311_P2.Models;
+
+namespace St10439541_PROG7311_P2.Controllers
+{
+    public class ServiceRequestsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiceRequestsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ServiceRequests
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.ServiceRequests.Include(s => s.Contract);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: ServiceRequests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceRequest = await _context.ServiceRequests
+                .Include(s => s.Contract)
+                .FirstOrDefaultAsync(m => m.ServiceRequestId == id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceRequest);
+        }
+
+        // GET: ServiceRequests/Create
+        public IActionResult Create()
+        {
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "ContractId", "ContractId");
+            return View();
+        }
+
+        // POST: ServiceRequests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiceRequestId,ContractId,Description,AmountUSD,AmountZAR,Status,CreatedAt,ExchangeRateUsed")] ServiceRequest serviceRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serviceRequest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "ContractId", "ContractId", serviceRequest.ContractId);
+            return View(serviceRequest);
+        }
+
+        // GET: ServiceRequests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceRequest = await _context.ServiceRequests.FindAsync(id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "ContractId", "ContractId", serviceRequest.ContractId);
+            return View(serviceRequest);
+        }
+
+        // POST: ServiceRequests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServiceRequestId,ContractId,Description,AmountUSD,AmountZAR,Status,CreatedAt,ExchangeRateUsed")] ServiceRequest serviceRequest)
+        {
+            if (id != serviceRequest.ServiceRequestId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(serviceRequest);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceRequestExists(serviceRequest.ServiceRequestId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "ContractId", "ContractId", serviceRequest.ContractId);
+            return View(serviceRequest);
+        }
+
+        // GET: ServiceRequests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceRequest = await _context.ServiceRequests
+                .Include(s => s.Contract)
+                .FirstOrDefaultAsync(m => m.ServiceRequestId == id);
+            if (serviceRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(serviceRequest);
+        }
+
+        // POST: ServiceRequests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var serviceRequest = await _context.ServiceRequests.FindAsync(id);
+            if (serviceRequest != null)
+            {
+                _context.ServiceRequests.Remove(serviceRequest);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceRequestExists(int id)
+        {
+            return _context.ServiceRequests.Any(e => e.ServiceRequestId == id);
+        }
+    }
+}
